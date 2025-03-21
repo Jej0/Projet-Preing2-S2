4a83d20d1402a461ab7875c2820a0e231b9ee371
Jej0 <76699284+Jej0@users.noreply.github.com>
2025-03-21 21:21:54 +0100

    php log

--- scripts_php/register.php
+++ scripts_php/register.php
@@ -5,29 +5,29 @@ session_start();
 define('USERS_FILE', 'users.json');
 
 // Fonction pour récupérer les utilisateurs depuis le fichier JSON
-function getUsers() {
-    if (!file_exists(USERS_FILE)) {
+function getUsers(): mixed {
+    if (!file_exists(filename: USERS_FILE)) {
         return [];
     }
     
-    $data = file_get_contents(USERS_FILE);
-    return json_decode($data, true) ?: [];
+    $data = file_get_contents(filename: USERS_FILE);
+    return json_decode(json: $data, associative: true) ?: [];
 }
 
 // Fonction pour enregistrer les utilisateurs dans le fichier JSON
-function saveUsers($users) {
-    file_put_contents(USERS_FILE, json_encode($users, JSON_PRETTY_PRINT));
+function saveUsers($users): void {
+    file_put_contents(filename: USERS_FILE, data: json_encode(value: $users, flags: JSON_PRETTY_PRINT));
 }
 
 // Traitement du formulaire d'inscription
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = trim($_POST['username']);
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
-    $confirm_password = trim($_POST['confirm-password']);
+    $username = trim(string: $_POST['username']);
+    $email = trim(string: $_POST['email']);
+    $password = trim(string: $_POST['password']);
+    $confirm_password = trim(string: $_POST['confirm-password']);
 
     if (!empty($username) && !empty($email) && !empty($password) && !empty($confirm_password)) {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var(value: $email, filter: FILTER_VALIDATE_EMAIL)) {
             echo 'Adresse email invalide !';
             exit;
         }
@@ -52,9 +52,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
         
         // Ajouter le nouvel utilisateur
-        $users[] = ['username' => $username, 'email' => $email, 'password' => password_hash($password, PASSWORD_DEFAULT)];
-        saveUsers($users);
-        echo 'Inscription réussie !';
+        $users[] = ['username' => $username, 'email' => $email, 'admin' => False, 'password' => password_hash(password: $password, algo: PASSWORD_DEFAULT)];
+        saveUsers(users: $users);
+
+        $_SESSION['user'] = [
+            'username' => $user['username'],
+            'email' => $user['email'],
+            'admin' => $user['admin']
+        ];
+        
+        // Rediriger vers la page d'accueil ou le tableau de bord
+        header(header: 'Location: ../accueil.php');
+        exit;
+
     } else {
         echo 'Veuillez remplir tous les champs !';
     }
